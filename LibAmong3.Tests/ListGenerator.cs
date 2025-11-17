3ca60337097d831e05327b8e63345f66f1df051d
kenjiuno <ku@digitaldolphins.jp>
2025-11-17 21:45:39 +0900

    Filterable

---
+++ LibAmong3.Tests/ListGenerator.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAmong3.Tests
+{
+    public class ListGenerator
+    {
+        [Test]
+        [Ignore("Private use")]
+        public void GenerateListOfMicrosoftWindowsOperatingSystemDLLs()
+        {
+            bool Test(string dllName)
+            {
+                try
+                {
+                    var versionInfo = FileVersionInfo.GetVersionInfo(dllName);
+                    var productName = versionInfo.ProductName ?? "";
+                    return true
+                        && productName.Contains("Microsoft")
+                        && productName.Contains("Windows")
+                        && productName.Contains("Operating")
+                        && productName.Contains("System")
+                        ;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            foreach (var file in Directory.GetFiles(Environment.SystemDirectory, "*.dll")
+                .Where(it => Test(it))
+            )
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+        }
+    }
+}
--- ReadPEImportTable/Program.cs
+++ ReadPEImportTable/Program.cs
@@ -1,15 +1,36 @@
 ﻿using CommandLine;
 using LibAmong3.Helpers.PE32;
+using System.Linq;
 
 namespace ReadPEImportTable
 {
     internal class Program
     {
+        private interface IFilterable
+        {
+            IEnumerable<string>? IncludeSets { get; }
+            IEnumerable<string>? ExcludeSets { get; }
+            IEnumerable<string>? Include { get; }
+            IEnumerable<string>? Exclude { get; }
+        }
+
         [Verb("print-import-dll", HelpText = "Print the imported DLLs from a PE file.")]
-        private class PrintImportDllOpt
+        private class PrintImportDllOpt : IFilterable
         {
             [Value(0, Required = true, HelpText = "Path to PE file.")]
             public string FilePath { get; set; } = null!;
+
+            [Option("include-sets", HelpText = "Only include sets of DLLs that match these filters.")]
+            public IEnumerable<string> IncludeSets { get; set; } = Array.Empty<string>();
+
+            [Option("exclude-sets", HelpText = "Exclude sets of DLLs that match these filters.")]
+            public IEnumerable<string> ExcludeSets { get; set; } = Array.Empty<string>();
+
+            [Option("include", HelpText = "Only include names of DLLs that match these filters.")]
+            public IEnumerable<string> Include { get; set; } = Array.Empty<string>();
+
+            [Option("exclude", HelpText = "Exclude names of DLLs that match these filters.")]
+            public IEnumerable<string> Exclude { get; set; } = Array.Empty<string>();
         }
 
         [Verb("print-import-entries", HelpText = "Print the imported entries from a PE file.")]
@@ -60,12 +81,62 @@ namespace ReadPEImportTable
 
         private static int DoPrintImportDll(PrintImportDllOpt o)
         {
+            var filter = BuildFilterFunc(o);
             var importTable = LoadImportTable(o.FilePath);
-            foreach (var entry in importTable)
+            foreach (var entry in importTable
+                .Where(entry => filter(entry.DLLName)))
             {
                 Console.WriteLine(entry.DLLName);
             }
             return 0;
+        }
+
+        private static Func<string, bool> BuildFilterFunc(IFilterable filterable)
+        {
+            var includeDLLNames = new HashSet<string>(
+                new string[0]
+                    .Concat(GetDLLNamesListOf(filterable.IncludeSets))
+                    .Concat(filterable.Include ?? Enumerable.Empty<string>()),
+                StringComparer.OrdinalIgnoreCase
+            );
+            var excludeDLLNames = new HashSet<string>(
+                new string[0]
+                    .Concat(GetDLLNamesListOf(filterable.ExcludeSets))
+                    .Concat(filterable.Exclude ?? Enumerable.Empty<string>()),
+                StringComparer.OrdinalIgnoreCase
+            );
+
+            return dllName =>
+            {
+                if (includeDLLNames.Any())
+                {
+                    bool included = includeDLLNames.Contains(dllName);
+                    if (!included)
+                    {
+                        return false;
+                    }
+                }
+
+                if (excludeDLLNames.Contains(dllName))
+                {
+                    return false;
+                }
+
+                return true;
+            };
+        }
+
+        private static IEnumerable<string> GetDLLNamesListOf(IEnumerable<string>? filters)
+        {
+            if (filters == null)
+            {
+                return Enumerable.Empty<string>();
+            }
+            else
+            {
+                return new string[0]
+                    .Concat(filters.Contains("windows10", StringComparer.OrdinalIgnoreCase) ? DLLNames.Windows10_x64_22H2_19045_6575_SystemDLLs : Enumerable.Empty<string>());
+            }
         }
     }
 }
