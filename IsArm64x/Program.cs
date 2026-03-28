4d43980e9951b1ceb570bd7282e294defc7dc097
kenjiuno <ku@digitaldolphins.jp>
2026-03-28 20:48:55 +0900

    IsArm64x/Program.cs

--- IsArm64x/Program.cs
+++ IsArm64x/Program.cs
@@ -10,6 +10,15 @@ namespace IsArm64x
         {
             [Value(0, MetaName = "Input", HelpText = "Specify a file path to DLL/EXE", Required = true)]
             public string? Input { get; set; }
+
+            [Option('d', "disable-arm64x-detection", HelpText = "Disable detection of Arm64X binaries.")]
+            public bool DisableArm64XDetection { get; set; }
+
+            [Option('p', "look-at-chpe-metadata-version", HelpText = "Look at CHPE metadata version to help determine the binary form.")]
+            public bool LookAtCHPEMetadataVersion { get; set; }
+
+            [Option('v', "look-at-dvrt-header", HelpText = "Look at DVRT header to help determine the binary form.")]
+            public bool LookAtDvrtHeader { get; set; }
         }
 
         static int Main(string[] args)
@@ -24,7 +33,15 @@ namespace IsArm64x
         private static int Do(Opt opt)
         {
             var bytes = File.ReadAllBytes(opt.Input ?? throw new ArgumentNullException("opt.Input"));
-            var result = new GuessArm64XBinaryHelper().Guess(bytes);
+            var result = new GuessArm64XBinaryHelper().Guess(
+                bytes,
+                new Guess1Options
+                {
+                    DisableArm64XDetection = opt.DisableArm64XDetection,
+                    LookAtCHPEMetadataVersion = opt.LookAtCHPEMetadataVersion,
+                    LookAtDvrtHeader = opt.LookAtDvrtHeader,
+                }
+            );
             Console.WriteLine(result);
             return 0;
         }
